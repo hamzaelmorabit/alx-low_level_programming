eb220f2e796955e7ae401720db41e304218d6be5
hamzaelmorabit <hamzamorabit123@gmail.com>
2023-04-03 17:34:23 -0700

    add description

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print using printf
+ *
+ * Return: Always 0
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
