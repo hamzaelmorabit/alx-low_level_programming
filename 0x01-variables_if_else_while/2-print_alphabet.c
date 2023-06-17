2e21c3468924f345acb2b5df5508e7a0a7d48ad7
hamza el morabit <hamzamorabit123@gmail.com>
2023-06-17 12:16:24 -0700

    add file for alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n satus; zero, positive or negative
+ *
+ * Return: return 0 if code succeed
+ */
+int main(void)
+{
+for (char c = 'a'; c <= 'z'; c++)
+    putchar(c);
+putchar('\n');
+return (0);
+}
