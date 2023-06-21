0a1b773e00a1760de0c753a4c7c9d5758a623b45
Hamza EL MORABIT <57451284+hamzaelmorabit@users.noreply.github.com>
2023-06-21 23:51:39 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+*/
+int add(int a, int b)
+{
+	int s = a + b;
+
+	return (s);
+}
