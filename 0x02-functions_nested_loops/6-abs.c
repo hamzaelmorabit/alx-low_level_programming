7ebe8a4908a834278309c65d56515615ffcc7694
Hamza EL MORABIT <57451284+hamzaelmorabit@users.noreply.github.com>
2023-06-21 23:49:35 +0100

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ * Description: computes the absolute value of an integer.
+ * Return: absolute value
+ * @i: Ascii code
+ */
+int _abs(int i)
+{
+if (i < 0)
+{
+return (-i);
+}
+return (i);
+}
