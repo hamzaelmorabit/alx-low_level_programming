9bee248d529523e76b2bc061fad41c820ef59258
Hamza EL MORABIT <57451284+hamzaelmorabit@users.noreply.github.com>
2023-06-21 23:47:20 +0100

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Description:  prints 10 times the alphabet, in lowercase
+ * Return: success
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char ch;
+for (i = 0 ; i < 10 ; i++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+}
