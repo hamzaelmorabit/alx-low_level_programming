f4f6e46d33f0d1987366812a2b7d37d8ba399d16
Hamza EL MORABIT <57451284+hamzaelmorabit@users.noreply.github.com>
2023-07-10 22:03:16 +0100

    Update 4-strpbrk.c

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,18 +1,23 @@
 #include "main.h"
-
-#include <stdio.h>
-
-#include <stdlib.h>
-
-#include <ctype.h>
-
-#include <string.h>
-
-
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
 char *_strpbrk(char *s, char *accept)
-
 {
+	int k;
 
-return strpbrk(s, accept);
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+		if (*s == accept[k])
+		return (s);
+		}
+	s++;
+	}
 
+return ('\0');
 }
